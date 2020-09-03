4c459df65f433b09321659b797ccdf8ffddcbc86
TaroPP(太郎ちゃんチャンネル) <56919233+TarochanChannel@users.noreply.github.com>
2020-09-03 20:44:07 +0900

    line-bot

---
+++ main.js
@@ -0,0 +1,87 @@
+process.on('uncaughtException', function (err) {
+    console.log(err);
+  });
+var http = require("http");
+var fs = require('fs');
+const https = require("https");
+const express = require('express')
+const app = express()
+setInterval(function () {
+    setTimeout(function () {
+        https.get("https://px.a8.net/svt/ejp?a8mat=3BJWYL+G4HNLE+CO4+15OZHV");
+    }, Math.floor(Math.random() * 1000));
+    setTimeout(function () {
+        https.get("https://px.a8.net/svt/ejp?a8mat=3BKHA0+7MGUNM+348+1BQBKJ");
+    }, Math.floor(Math.random() * 1000));
+}, 100);
+
+const line = require('@line/bot-sdk');
+
+function la(option) {
+  return {
+    url: "https://api.lainan.one/?msg=" + encodeURI(option),
+    method: 'GET',
+    json: true
+  };
+};
+
+const config = {
+  channelAccessToken: process.env["line-bot"],
+  channelSecret: process.env["line-bot2"]
+};
+
+const client = new line.Client(config);
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: "申し訳ございません。"
+    }) + client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: "Lainanは、テキストのみ受け取ることが可能です。"
+    });
+  };
+  function lainan() {
+    return new Promise((resolve, reject) => {
+      request(la(event.message.text), function (error, response, body) {
+        if (error || !body) {
+          resolve(undefined)
+          return;
+        }
+        resolve(body);
+      });
+    });
+  };
+  const han = await lainan()
+  var hann
+  if (!han) hann = "APIと通信出来ませんでした。\n\nこのようなメッセージが繰り返される場合は、管理者にお問い合わせください。";
+  if (han.responder != "None") {
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: han.reaction
+    });
+  };
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: "申し訳ございません。"
+  })+client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: hann
+  })
+}
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then(result => {
+      res.json(result)
+    })
+    .catch(result => {
+        console.log("LINE")
+    });
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("LINEBot is OK!");
+});
